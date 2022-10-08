b6f2fc34fd4664ecb79bb6715f6be3754a0f0cb0
MekhrojDoliev <thugsjon708@gmail.com>
2022-10-08 13:58:52 +0200

    Scaffold function App bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text(
-          "Hello Flutter",
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          textDirection: TextDirection.ltr,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Flutter App"),
         ),
       ),
     ),
