aad8227ccd069eed5a1aac514a0345bd1ef9c8a3
MekhrojDoliev <thugsjon708@gmail.com>
2022-10-08 14:10:04 +0200

    Scaffold Body

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,17 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.indigo,
         appBar: AppBar(
           title: Text("My Flutter App"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text(
+            "This is my Homepage",
+            style: TextStyle(
+                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
         ),
       ),
     ),
