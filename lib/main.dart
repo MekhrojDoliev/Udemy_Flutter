9dccc4987f5871888f72a9ded1de543a850ba229
MekhrojDoliev <thugsjon708@gmail.com>
2022-10-09 06:16:34 +0200

    Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyFirstApp());
+}
+
+class MyFirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.indigo,
         appBar: AppBar(
-          title: Text("My Flutter App"),
+          title: Text("My first App"),
           centerTitle: true,
         ),
-        body: Center(
-          child: Text(
-            "This is my Homepage",
-            style: TextStyle(
-                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-        ),
       ),
-    ),
-  );
+    );
+  }
 }
