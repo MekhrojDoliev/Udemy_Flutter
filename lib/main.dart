e8d0a52439c0d60b6dac9367c464f009946eb6d0
MekhrojDoliev <thugsjon708@gmail.com>
2022-10-10 09:50:37 +0200

    Layout Widgets Padding

--- lib/main.dart
+++ lib/main.dart
@@ -8,31 +8,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: "IndieFlower"),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Adding Assets"),
+          title: Text("Layout Widgets"),
           centerTitle: true,
         ),
-        body: Center(
-          child: Stack(
-            fit: StackFit.expand,
-            children: <Widget>[
-              Image(
-                image: AssetImage("assets/images/bg.jpg"),
-              ),
-              Image.asset("assets/icon.png"),
-              Positioned(
-                top: 16,
-                left: 115,
-                child: Text(
-                  "My custom Font",
-                  style: TextStyle(fontSize: 30, color: Colors.white)//fontFamily: "IndieFlower"),
-                ),
-              ),
-            ],
-          ),
+        body: Padding(
+          padding: EdgeInsets.only(left: 100,top: 150),
+          child: Text(
+          "Hello Flutter",
+          style: TextStyle(fontSize: 30),
         ),
+        ),
+        
+        
       ),
     );
   }
